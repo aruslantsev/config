402aa9047544f1262b1e0e94faa09741de20cf39
aruslantsev <andreiruslantsev@gmail.com>
2021-12-29 22:11:46 +0300

    system update

--- scripts/usr/reverse_conn_ssh.sh
+++ scripts/usr/reverse_conn_ssh.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
-
 if [ -z "$1" ]
 then
-        HOST="oracle-ssh-gw"
+	HOST="oracle-ssh-gw"
 else
-        HOST=$1
+	HOST=$1
 fi
 
 if [ -z "$2" ]
 then
-        PORT=10022
+	PORT=10022
 else
-        PORT=$2
+	PORT=$2
 fi
 
 if [ -z "$3" ]
 then
-        INTERVAL=300
+	INTERVAL=300
 else
-        INTERVAL=$3
+	INTERVAL=$3
 fi
 
 CMD1="ssh -f -N -R ${PORT}:localhost:22 ${HOST}"
 
 echo "Executing $CMD1, refresh interval: $INTERVAL seconds"
 
-trap "echo \"Caught SIGINT\"; pgrep -f \"$CMD1\" | xargs kill; exit" SIGINT
+trap "echo \"Caught SIGINT\"; pgrep -f \"$CMD1\" | xargs kill; ssh-agent -k; exit" SIGINT
+
+eval $(ssh-agent)
+ssh-add
 
 while true
 do
-        pgrep -f "$CMD1" &>/dev/null && echo "$(date +'%Y.%m.%d %H:%M:%S'): OK" || ( echo "$(date +'%Y.%m.%d %H:%M:%S'): Reconnecting..."; $CMD1 & )
-        sleep $INTERVAL
+	pgrep -f "$CMD1" &>/dev/null && echo "$(date +'%Y.%m.%d %H:%M:%S'): OK" || ( echo "$(date +'%Y.%m.%d %H:%M:%S'): Reconnecting..."; $CMD1 & )
+	sleep $INTERVAL
 done
 
 # on ssh-gw ssh -p 10022 USER@localhost
