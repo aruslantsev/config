7e395ca2cd59a86eff5acb0578432459913be196
Andrei Ruslantsev <andreiruslantsev@gmail.com>
2023-12-19 21:53:12 +0200

    update

--- Gentoo/scripts/stage4/mkstage4.sh
+++ Gentoo/scripts/stage4/mkstage4.sh
@@ -1,7 +1,7 @@
 mount -o bind / /mnt
 mount -o bind /boot /mnt/boot
 cd /mnt/
-tar cjpvf ~/stage4.tar.bz2 -X ~/stage4.excl .
+tar -cpP --xattrs-include=*.* --numeric-owner --checkpoint=10000 --selinux -jf ~/stage4.tar.bz2 -X ~/stage4.excl .
 cd
 umount /mnt/boot
 umount /mnt
